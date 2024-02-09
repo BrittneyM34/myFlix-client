4a4a5af30b435ad5e5af6ce9c3b7986fffef06ef
BrittneyM34 <brittneym1018@yahoo.com>
2024-02-08 20:25:45 -0500

    update director info

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,18 +1,34 @@
-export const MovieView = ({ movie, onBackClick}) => {
+export const MovieView = ({ movie, onBackClick }) => {
     return (
+      <div>
         <div>
-            <div>
-                <img src={movie.image} />
-            </div>
-            <div>
-                <span>Title: </span>
-                <span>{movie.title}</span>
-            </div>
-            <div>
-                <span>Director: </span>
-                <span>{movie.director}</span>
-            </div>
-            <button onClick={onBackClick}>Back</button>
+          <img src={movie.image} />
         </div>
+        <div>
+          <span>
+            <b>Title:</b>
+          </span>
+          <span>{movie.title}</span>
+        </div>
+        <div>
+          <span>
+            <b>Description: </b>
+          </span>
+          <span>{movie.description}</span>
+        </div>
+        <div>
+          <span>
+            <b>Genre: </b>
+          </span>
+          <span>{movie.genre}</span>
+        </div>
+        <div>
+          <span>
+            <b>Director: </b>
+          </span>
+          <span>{movie.director}</span>
+        </div>
+        <button onClick={onBackClick}>Back</button>
+      </div>
     );
-};
+  };
