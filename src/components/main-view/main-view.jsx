449bbec4ae8053642bf8773eddb11c9952891a18
BrittneyM34 <brittneym1018@yahoo.com>
2024-02-13 10:48:12 -0500

    update login view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,37 +1,63 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
     const [movies, setMovies] = useState([]);
-
     const [selectedMovie, setSelectedMovie] = useState(null);
+    const [user, setUser] = useState(storedUser ? storedUser : null);
+    const [token, setToken] = useState(storedToken ? storedToken : null);
 
     useEffect(() => {
-        fetch("https://my-movies-8ed51d856f3e.herokuapp.com/movies")
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            const moviesFromApi = data.map((movie) => {
-                return {
-                    imagePath: movie.imagePath || 'default-image-path.jpg', 
-                    _id: movie._id,
-                    title: movie.title,
-                    description: movie.description,
-                    genre: {
-                        name: movie.genre.name,
-                        description: movie.genre.description
-                      },
-                      director: {
-                        name: movie.director.name,
-                        bio: movie.director.bio,
-                        birth: movie.director.birth
-                      },
+        if (!token) {
+            return;
+        }
+
+        fetch("https://my-movies-8ed51d856f3e.herokuapp.com/movies", {
+            headers: { Authorization: `Bearer ${token}` },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                // debug
+                console.log(data);
+                const moviesFromApi = data.map((movie) => {
+                    return {
+                        imagePath: movie.imagePath || 'default-image-path.jpg',
+                        _id: movie._id,
+                        title: movie.title,
+                        description: movie.description,
+                        genre: {
+                            name: movie.genre.name,
+                            description: movie.genre.description
+                        },
+                        director: {
+                            name: movie.director.name,
+                            bio: movie.director.bio,
+                            birth: movie.director.birth
+                        },
                     };
-                  });
-            setMovies(moviesFromApi);
-        });
-    }, []);
+                });
+                setMovies(moviesFromApi);
+            });
+    }, [token]);
+
+    if (!user) {
+        return (
+            <>
+                <LoginView onLoggedIn={(user, token) => {
+                    setUser(user);
+                    setToken(token);
+                }}
+                />
+                or
+                <SignupView />
+            </>
+        );
+    }
 
     if (selectedMovie) {
         return (
@@ -43,7 +69,19 @@ export const MainView = () => {
     }
 
     if (movies.length === 0) {
-        return <div>The list is empty!</div>;
+        return (
+            <>
+                <button onClick={() => {
+                    setUser(null);
+                    setToken(null);
+                    localStorage.clear();
+                }}
+                >
+                    Logout
+                </button>
+                <div>The list is empty!</div>;
+            </>
+        );
     }
 
     return (
@@ -57,6 +95,14 @@ export const MainView = () => {
                     }}
                 />
             ))}
+            <button onClick={() => {
+                setUser(null);
+                setToken(null);
+                localStorage.clear();
+            }}
+            >
+                Logout
+            </button>
         </div>
     );
 };
