db0b0b48d064c60bc24f2d3e2ac2a0d81638dbe8
BrittneyM34 <brittneym1018@yahoo.com>
2024-02-15 19:08:43 -0500

    added 3.6

--- src/index.jsx
+++ src/index.jsx
@@ -19,3 +19,27 @@ const root = createRoot(container);
 
 // Tells React to render your app in the root DOM element
 root.render(<MyFlixApplication />);
+
+// import { createRoot } from "react-dom/client";
+// import { MainView } from "./components/main-view/main-view";
+// import { Container } from "react-bootstrap";
+
+// // Import statement to indicate that you need to bundle `./index.scss`
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "./index.scss";
+
+// // Main component (will eventually use all the others)
+// const MyFlixApplication = () => {
+//   return (
+//     <Container>
+//       <MainView />
+//     </Container>
+//   );
+// };
+
+// // Finds the root of your app
+// const container = document.querySelector("#root");
+// const root = createRoot(container);
+
+// // Tells React to render your app in the root DOM element
+// root.render(<MyFlixApplication />);
